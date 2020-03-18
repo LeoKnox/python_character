517b41ca156c107f6f85921c3432a35df8bd369d
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-18 12:20:19 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -15,6 +15,14 @@ def create():
 def character():
     return render_template("character.html", character="active")
 
+@app.route("/spells")
+def spells():
+    spellData = [{"spellID":"1111","title":"Fireball","casting":8,"type":"fire"},
+        {"spellID":"1112","title":"Stunning Touch","casting":6,"type":"lightning"},
+        {"spellID":"1113","title":"Snow Ball","casting":3,"type":"ice"},
+        {"spellID":"1114","title":"Summon Demon","casting":4,"type":"demon"}]
+    return render_template("spells.html", spellData=spellData, spells="active")
+
 @app.route("/login")
 def login():
     return render_template("login.html", login="active")
