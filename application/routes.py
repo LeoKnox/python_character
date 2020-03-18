65eeca2513d0c2b34e3d6c68710a558ff26d41cd
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-17 22:22:22 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -3,5 +3,18 @@ from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
     return render_template("index.html", home="active")
+
+@app.route("/create")
+def create():
+    return render_template("create.html", home="active")
+
+@app.route("/character")
+def character():
+    return render_template("character.html", home="active")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", home="active")
