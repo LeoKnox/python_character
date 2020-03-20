bcfc18c37de3525dfbbfac87f7addb853d2c7cce
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-19 22:39:09 -0700

    Add files via upload

--- application/forms.py
+++ application/forms.py
@@ -2,3 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
+class LoginForm(FlaskForm):
+    char_name   =   StringField("Character Name", validators=[DataRequired()])
+    char_class  =   StringField("Character Class", validators=[DataRequired()])
+    remember_me =   BooleanField("Remember Me")
+    submit = SubmitField("Login")
+
+class CreateForm(FlaskForm):
+    char_name   =   StringField("Character Name", validators=[DataRequired()])
+    char_class  =   StringField("Class", validators=[DataRequired()])
+    confirm_class = StringField("Confirm Class", validators=[DataRequired()])
+    char_atk    =   IntField("Attack", validators=[DataRequired()])
+    char_def    =   IntField("Defense", validators=[DataRequired()])
+    char_hp     =   IntField("Hit Points", validators=[DataRequired()])
+    sumbit      =   SubmitField("Create Now")
