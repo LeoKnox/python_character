c837256c8a2c234597440de41188789e2da79c15
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-23 23:44:29 -0700

    Add files via upload

--- application/routes_dungeon.py
+++ application/routes_dungeon.py
@@ -4,4 +4,5 @@ from flask import render_template, request, redirect, flash, url_for, session
 @app.route('/dungeon')
 def dungeon():
     room = {"length":15, "width":15}
-    return render_template("dungeon.html", room=room)
+    player = {"locx":3, "locy":5}
+    return render_template("dungeon.html", room=room, player=player)
